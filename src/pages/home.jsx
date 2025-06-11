284f7f95b25497389e5a85c1de50f478f71d5cd3
JohnLouisMaker <jluisilvenancio@gmail.com>
2025-06-11 15:38:29 -0300

    test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,6 @@ import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 import mySelf from "../assets/Myself.png";
 
-import "./Home.css";
 
 const card =
   "bg-gray-900 py-8 px-6 rounded-xl shadow-lg w-35 h-50 sm:w-40 sm:h-60 md:w-50 md:h-75 lg:w-50 h-44 flex flex-col items-center justify-center text-center hover:scale-105 transform transition-all duration-200 opacity-0 translate-y-10 animate-slide-up";
